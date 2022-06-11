e84387091da40da68611bd0b8b050b98812bf2dd
Allpin23 <106477620+Allpin23@users.noreply.github.com>
2022-06-11 11:20:16 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,21 +18,21 @@ global.autoread = false // auto read pesan / message
 //global.available = false //status online (online)
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.botname = "AlpinBotz" //namabot kalian
+global.ownername= "Pemilik bot: 089648311511" //nama kalian
+global.myweb ="https://chat.whatsapp.com/Dse5mkgbOetEX7a0Rvofjh" //bebas asal jan hapus
+global.youtube = "https://chat.whatsapp.com/Dse5mkgbOetEX7a0Rvofjh" //bebas asal jan hapus
+global.github = "https://chat.whatsapp.com/Dse5mkgbOetEX7a0Rvofjh" //bebas
+global.email = "alfinabdilaha@gmail.com" //bebas
+global.region = "Merkurius" //bebas
+global.ownernomer = "6289648311511" // nomor wa kalian
+global.ownernomerr = "+6289648311511" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62887435047326","62887435047326","918156874290"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.owner = ["6289648311511"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© AlpinBotz' //sticker wm ubah
+global.author = 'Di Buat Oleh AlphinBotz' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
